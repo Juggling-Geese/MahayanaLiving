fa2a3f34feecdfa48f0412fec12de3712b39784e
Brooke Loftus <brooke@zendeavors.net>
2019-12-30 17:01:46 -0700

    Updates to db connection info

--- senddata.php
+++ senddata.php
@@ -1,39 +1,40 @@
-<!doctype html>
-<html>
-<head>
-    <title>Test</title>
-</head>
-<body>
-    <p>Hello World</p>
-    
+
 <?php
 error_reporting(E_ALL);
 
-$name = "'name'";
-$address1 = "'address1'";
-$address2 = "'address2'";
-$address3 = "'address3'";
-$email = "'email'";
-$phone = "'phone'";
-$comments = "'comments'";
+require 'db.php';
+
 $errors = 0;
+
+function customerInfo(){
+    $customerInfo = array(
+        'name' => "'name'",
+        'address1' => "'address1'",
+        'address2' => "'address2'",
+        'address3' => "'address3'",
+        'email' => "'email'",
+        'phone' => "'phone'",
+        'comments' => "'comments'"
+    );
+    return $customerInfo;
     
-$connectRemote = array('host' => 'localhost',
-                'username' => 'zendeavo_writer',
-                'pwd' => 'Writer69!',
-                'DBName' => 'zendeavo_contacts');
+}
+
     
-$connectLocal = array('host' => 'localhost',
-                     'username' => 'root',
-                     'pwd'=>'CWB208',
-                     'DBName' => 'customers');
-    
-function connectToDatabae(){
-    
+function connectToDatabase(){
+    $connectLocal = connectLocal();
+    $connectRemote = connectRemote();
+    global $errors;
+
     $DBConnect = new mysqli($connectLocal['host'], 
                             $connectLocal['username'], 
                             $connectLocal['pwd'], 
                             $connectLocal['DBName']);
+
+    // $DBConnect = new mysqli($connectRemote['host'], 
+    //                         $connectRemote['username'], 
+    //                         $connectRemote['pwd'], 
+    //                         $connectRemote['DBName']);
 
     if( $DBConnect-> connect_error){
         ++$errors;
@@ -44,15 +45,17 @@ function connectToDatabae(){
 }
     
 function sendToDatabase(){
-    
+    global $errors;
+    $customerInfo = customerInfo();
+
     $DBConnect = connectToDatabase();
     
     $Table = 'customers';
 
-    if( 'errors == 0' ){ // && isset($_POST[])){
+    if( $errors == 0 ){ // && isset($_POST[])){
 
         $SQLQuery = 'insert into ' . $Table . 
-            '(name, streetaddress1, streetaddress2, citystatezip, email) values (' . $name . ',' . $address1 . ',' . $address2 . ',' . $address3 . ',' . $email . ')';
+            '(name, streetaddress1, streetaddress2, citystatezip, email) values (' . $customerInfo('name') . ',' . $customerInfo('address1') . ',' . $customerInfo('address2') . ',' . $customerInfo('address3') . ',' . $customerInfo('email') . ')';
 
         //var_dump($SQLQuery);
         if( $DBConnect->query($SQLQuery) === true){
@@ -61,7 +64,7 @@ function sendToDatabase(){
             $_SESSION['userid'] = $newID;
 
             $Table = 'comments';
-            $SQLQuery = "insert into " . $Table . " (customerid, comments) values (" . $newID . ", " . $comments . ")";
+            $SQLQuery = "insert into " . $Table . " (customerid, comments) values (" . $newID . ", " . $customerInfo('comments') . ")";
 
             if( $DBConnect->query($SQLQuery) === true){
                 echo "Comment inserted! ";
@@ -77,16 +80,13 @@ function sendToDatabase(){
     }else
         ++$errors;
 
-    mysql_close($DBConnect);
+    mysqli_close($DBConnect);
 }
     
 function emailContactInfo(){
     $name = "'name'";
-    $address1 = "'address1'";
-    $address2 = "'address2'";
     $address3 = "'address3'";
     $email = "'email'";
-    $phone = "'phone'";
     $comments = "'comments'";
     
     $mailTo = "brooke@zendeavors.net";
@@ -98,8 +98,8 @@ function emailContactInfo(){
     "Location: " . $address3 . "\n" . 
     "Comments: " . $comments;
 
-   if( mail($mailTo,$subject,$message_body,"From: ".$from) )
-    echo "Mail has been sent";
+    if( mail($mailTo,$subject,$message_body,"From: ".$from) )
+        echo "Mail has been sent";
 
 }
 
